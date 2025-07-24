bc7fa6fcd6e107a39c681ff91d28f8f5a9207a80
Bereket Gezhagne <bereketgezhagne@gmail.com>
2025-07-24 19:45:58 +0300

    Update Project.js

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import ProjectCard from './ProjectCard';
+import projectsData from './../data/projectsData'; // Import your project data
+import './../styles/Projects.css';
+
+function Projects() {
+  return (
+    <section id="projects" className="projects-section">
+      <h2>My Projects</h2>
+      <div className="projects-grid">
+        {projectsData.map(project => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
 
