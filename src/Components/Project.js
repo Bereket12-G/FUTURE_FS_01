32c455dc0ba760bc6f56872a8601c21227954d97
Bereket Gezhagne <bereketgezhagne@gmail.com>
2025-07-24 20:30:49 +0300

    Update Project.js

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -6,7 +6,7 @@ import './../styles/Projects.css';
 function Projects() {
   return (
     <section id="projects" className="projects-section">
-      <h2>My Projects</h2>
+      <h2>My Projectsbbbnnjnjj</h2>
       <div className="projects-grid">
         {projectsData.map(project => (
           <ProjectCard key={project.id} project={project} />
