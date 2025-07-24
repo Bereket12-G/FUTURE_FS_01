7828fb630242fb2ef4a0861043f81b4cf9f30b95
Bereket Gezhagne <bereketgezhagne@gmail.com>
2025-07-24 19:37:22 +0300

    Update header.js

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,1 +1,21 @@
 
+import React from 'react';
+import './../styles/Header.css'; // Link to your CSS file for this component
+
+function Header() {
+  return (
+    <header className="header">
+      <nav className="navbar">
+        <a href="/" className="logo">Your Name</a>
+        <ul className="nav-links">
+          <li><a href="#about">About</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
+          {/* Optional: <li><a href="#blog">Blog</a></li> */}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
