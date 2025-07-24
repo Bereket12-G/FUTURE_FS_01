c4c2c10d53379b298365a0cf3aee8ae3478de306
Bereket Gezhagne <bereketgezhagne@gmail.com>
2025-07-24 19:48:42 +0300

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import './../styles/Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; {new Date().getFullYear()} Your Name. All rights reserved.</p>
+        <div className="footer-links">
+          <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          <a href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer">GitHub</a>
+          {/* Add more footer links if desired */}
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
 
